98dfacef61a0527366668e1da23b4615ef0d3547
Annedrew <1196244890@qq.com>
2024-05-20 20:28:50 +0200

    Add app icon and loading animation.

--- App.tsx
+++ App.tsx
@@ -1,15 +1,19 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from './src/screens/HomeScreen';
 import PaymentScreen from './src/screens/PaymentScreen';
 import DetailsScreen from './src/screens/DetailsScreen';
 import TabNavigator from './src/navigators/TabNavigator';
+import SplashScreen from 'react-native-splash-screen';
 
 const stack = createNativeStackNavigator();
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <NavigationContainer>
       <stack.Navigator screenOptions={{headerShown: false}}>
